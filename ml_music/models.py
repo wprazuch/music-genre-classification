1b4ba48b88e842e9473fd10da808334d494c74a9
WojPrauch <wojciechprazuch3@gmail.com>
2020-10-28 09:34:12 +0100

    Created initial architecture of the DenseNet (to be checked), created package for common functions used in the project

---
+++ ml_music/models.py
@@ -0,0 +1,51 @@
+from tensorflow.keras import layers
+from tensorflow.keras import Model, Sequential
+from tensorflow.keras.layers import Input, BatchNormalization, Dense, Dropout
+
+
+class DenseBlock(layers.Layer):
+    def __init__(self, no_neurons, dropout_rate=0.5, activation='relu'):
+
+        super(DenseBlock, self).__init__()
+
+        self.fc = layers.Dense(no_neurons, activation=activation)
+        self.drop = layers.Dropout(dropout_rate)
+        self.bn = layers.BatchNormalization()
+
+    def call(self, inputs, training=True):
+        x = self.fc(inputs)
+        x = self.drop(x, training=training)
+        x = self.bn(x, training=training)
+
+        return x
+
+
+class DenseNet(Model):
+
+    def __init__(
+            self, input_shape, no_classes, layer_sizes=[256, 256, 256],
+            dropout_rate=0.5, activation='relu'):
+        super(DenseNet, self).__init__()
+
+        self.inp = layers.Input(shape=(None, input_shape))
+
+        self.dense_blocks = []
+
+        for no_neurons in layer_sizes:
+            d_block = DenseBlock(no_neurons=no_neurons,
+                                 dropout_rate=dropout_rate, activation=activation)
+
+            self.dense_blocks.append(d_block)
+
+        self.out_dense = Dense(no_classes)
+
+    def call(self, inputs, training=True):
+
+        x = inputs
+
+        for dense_block in self.dense_blocks:
+            x = dense_block(x, training=training)
+
+        x = self.out_dense(x)
+
+        return x
---
+++ ml_music/utils.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+def one_hot_encode(x):
+    """
+    One hot encode a list of sample labels. Return a one-hot encoded vector for each label.
+    : x: List of sample Labels
+    : return: Numpy array of one-hot encoded labels
+     """
+    return np.eye(x.max()+1)[x]
